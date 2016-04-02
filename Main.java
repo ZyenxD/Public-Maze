f8e597d8d1e47295cdcd6340c5913616f7aa0344
ZyenxD <neyc30@gmail.com>
2016-04-02 12:56:36 -0400

    el codigo esta comentado y lo que hay que hacer esta marcado con TODO en las clases Main y MThreads, tambien, ya no usaremos las clases Cords y MazeThreads

--- Main.java
+++ Main.java
@@ -3,11 +3,32 @@ package maze;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+		// en el main se inicializan todas las clases
 		Maze m = new Maze();
-		MazeThreads mt = new MazeThreads();
+		// aqui se crea el labreinto
 		m.MazeCrwator();
-		mt.mazesolver();
+		// aqui se inicia el hilo en la dirrecion a la que ira
+		//TODO: validar forma para predecir en que dirreccion mandar el hilo inicial
+		//       y verificar si debe crear mas de un hilo en otra direccion apartir de la inicial
+		// lol eso sono como joa xD
+		Thread t = new Thread(new MThreads(m.en_corx,m.en_cory, m.maze, "S"));
+		t.start();
+		try {
+			t.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		for(int i =0;i<ThreadArr.ways.size();i++){
+			ThreadArr.ways.get(i).start();
+			try {
+				ThreadArr.ways.get(i).join();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 
 	}
 
--- Maze.java
+++ Maze.java
@@ -2,23 +2,39 @@ package maze;
 
 import java.io.*;
 
+/*
+ * esta clase lee el .txt y guarda la entrada, la salida;
+ * tambien guarda el laberinto en una matriz
+ * 
+ * por ahora solo acepta numeros de 1 digito como tamano de la mattriz
+ */
+
+/*
+ * TODO: impelmntar forma para contar cuantos pasos se dieron hasta la salida
+ * 		 tambien, arreglar algunos errores numericos en los print
+ */
+
 public class Maze {
+
+	static int[][] maze; // matriz donde se guarda el laberinto
+	static File Filemaze = new File("maze.txt"); // accseo al archivo
+	public static int en_corx,en_cory,e_corx,e_cory; // corrdenadas de entrada y salida
 	
-	static int[][] maze;
-	static File Filemaze = new File("maze.txt");
-	public static int en_corx,en_cory,e_corx,e_cory;
-	
+	//metodo para crear el laberinto
 	public void MazeCrwator(){
-		int size=0;
+		int size=0;// size del laberinto
 		
 		if(Filemaze.exists()){
 			try{
 				BufferedReader br = new BufferedReader(new FileReader(Filemaze));
-				String line= null;
-				int checkl =1,cont=0,ch=0;
+				String line= null; // guarda cada line del txt
+				int checkl =1,cont=0,ch=0; 
 				char n;
+				// este while se ejecuta por lineas y guarda la linea del documnto en line
 				while((line = br.readLine())!= null){
+					// check es un contador para verificar que linea esta leyendo
 					switch (checkl) {
+					// en el primer caso 1 le el size del arreglo
 					case 1:
 						n = line.charAt(5);
 						size = Character.getNumericValue(n);
@@ -26,6 +42,7 @@ public class Maze {
 						maze = new int[size][size];
 						checkl++;
 						break;
+						//en el caso 2 la cordenada de entrada
 					case 2:
 						en_corx = Character.getNumericValue(line.charAt(6));
 						en_cory = Character.getNumericValue(line.charAt(8));
@@ -33,6 +50,7 @@ public class Maze {
 						
 						checkl++;
 						break;
+						// en el caso 3 la coordenada de salida
 					case 3:
 						e_corx = Character.getNumericValue(line.charAt(5));
 						e_cory = Character.getNumericValue(line.charAt(7));
@@ -41,6 +59,7 @@ public class Maze {
 						checkl++;
 						break;
 					default:
+						// el resto de las lineas son el laberinto el cual se almacena en una matriz
 						for(int c2nt=0;cont<maze.length && c2nt<maze[cont].length;c2nt++){
 								maze[cont][c2nt] = Character.getNumericValue(line.charAt(ch));
 								maze[e_corx-1][e_cory-1] = 0;
